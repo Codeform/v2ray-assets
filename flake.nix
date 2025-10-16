8aa67340580022875e6166071afe7385a59e2112
nixos <nixos>
2025-10-16 09:21:38 +0800

    [chore] Bump V2Ray assets: 202510082212 -> 202510152212

--- flake.nix
+++ flake.nix
@@ -15,12 +15,12 @@
   in {
     packages.${system}.default = pkgs.stdenv.mkDerivation rec {
       name = "v2ray-assets";
-      version = "202510082212";
+      version = "202510152212";
       srcs = let
         geositeRev = version;
-        geositeHash = "sha256-5p8cH7Wf9HFRT54X6AW8b5NEUmw9WJEJpOFtUiRGUHc=";
+        geositeHash = "sha256-YNTcsMmpfW1Wu9Mr+ClOvHrjC9uxqgeXNnR3pIApseg=";
         geoipRev = version;
-        geoipHash = "sha256-rBCMjz8RDfnvZ68HcJ8qJlmbTOIFcOauUc+CzLSV4O0=";
+        geoipHash = "sha256-ciHscprbazaTgONNe0OWFZ0MhO8dNlP8rbxAT/4JObw=";
       in [
         (fetchurl {
           url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${geoipRev}/geoip.dat";
